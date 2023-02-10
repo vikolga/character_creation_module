b3ec7f0ada62989c5279cd7b83a0cb57cf6e8419
viktorova olga <viktotovaol4ik@yandex.ru>
2023-02-10 12:04:16 +0300

    Design and literals

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Определяем урон нанесенный противнику при атаке."""
     if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику равный'
-                f'{5 + randint(3, 5)}')
+        damade = 5 + randint(3, 5)
+        return (f'{char_name} нанёс урон противнику равный {damade}')
     if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный'
-                f'{5 + randint(5, 10)}')
+        damade = 5 + randint(5, 10)
+        return (f'{char_name} нанёс урон противнику равный {damade}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный'
-                f'{5 + randint(-3, -1)}')
+        damade = 5 + randint(-3, -1)
+        return (f'{char_name} нанёс урон противнику равный {damade}')
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Уровень защиты героя."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал'
                 f'{10 + randint(5, 10)}'
@@ -25,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Специальные умения героя."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80+25}»')
@@ -37,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка героя."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -49,7 +54,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или special — чтобы '
           'использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -62,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Создание героя."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -83,7 +89,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -93,6 +100,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
